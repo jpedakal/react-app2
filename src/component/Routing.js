a4870a504622720993d7a7108856a32b91561fad
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-07 21:47:30 +0530

    added css

--- src/component/Routing.js
+++ src/component/Routing.js
@@ -6,11 +6,18 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 const Routing = () => {
     return (
         <BrowserRouter>
-            <div className="container-fluid">
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/profile">Profile</Link></li>
-                </ul>
+            <div>
+                <nav classNameName="navbar navbar-inverse">
+                    <div classNameName="container-fluid">
+                        <div classNameName="navbar-header">
+                            <a className="navbar-brand" href="#">Employee Management</a>
+                        </div>
+                        <ul className="nav navbar-nav">
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/profile">Profile</Link></li>
+                        </ul>
+                    </div>
+                </nav>
                 <Route exact path="/" component={Home}></Route>
                 <Route path="/profile" component={Profile}></Route>
             </div>
