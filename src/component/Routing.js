5f8b9ab7e57fbfe76b4e09aa7af2df4d0a9c44af
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-08 20:31:05 +0530

    added post component

--- src/component/Routing.js
+++ src/component/Routing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './Home';
 import Profile from './Profile';
+import Post from './Post';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const Routing = () => {
@@ -15,11 +16,13 @@ const Routing = () => {
                         <ul className="nav navbar-nav">
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="/profile">Profile</Link></li>
+                            <li><Link to="/post">Post</Link></li>
                         </ul>
                     </div>
                 </nav>
                 <Route exact path="/" component={Home}></Route>
                 <Route path="/profile" component={Profile}></Route>
+                <Route path="/post" component={Post}></Route>
             </div>
         </BrowserRouter>
     )
