1d85ef7340491602205be7b40d8b6b7a9addf6a3
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-09 18:51:20 +0530

    Update PostDetails.js

--- src/component/PostDetails.js
+++ src/component/PostDetails.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const PostDetails = (props) => {
-    
+    console.log(props);
     return(
         <div className="panel panel-info">
         <div className="panel-heading">
