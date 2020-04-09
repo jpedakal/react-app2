74f9cf7b6e786f923e16782ac59a7e3fd5657808
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-04-09 08:47:56 +0530

    update new components

---
+++ src/component/lifecycle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LifeCycle= ()=>{
+    return(
+        <div>
+            <h3>
+                Life cycle page;
+            </h3>
+        </div>
+    )
+}
+
+export default LifeCycle;
